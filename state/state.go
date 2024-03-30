543d35793f676c4f488ef9051137c50978878d66
Your Name <you@example.com>
2024-03-30 22:03:57 +0400

    check user existance

--- state/state.go
+++ state/state.go
@@ -1,5 +1,7 @@
 package state
 
+import "fmt"
+
 // TODO: does it belong here?
 type User struct {
 	Account string
@@ -16,6 +18,12 @@ func New() *State {
 }
 
 func (s *State) Register(account string, name string, id uint8) error {
+	for _, v := range s.users {
+		if v.Account == account {
+			return fmt.Errorf("user with that account already exists: %s", account)
+		}
+	}
+	
 	s.users = append(s.users, &User{
 		Account: account,
 		Name:    name,
