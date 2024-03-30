5281ce4f518a52858332a3e2226de9e4517f7946
Your Name <you@example.com>
2024-03-30 23:31:50 +0400

    fix team tests

--- core/team/team_test.go
+++ core/team/team_test.go
@@ -6,6 +6,7 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"github.com/wdipax/match/core/player"
+	"github.com/wdipax/match/core/session"
 	"github.com/wdipax/match/core/team"
 )
 
@@ -15,15 +16,18 @@ func TestTeam(t *testing.T) {
 	t.Run("it registers a new user", func(t *testing.T) {
 		t.Parallel()
 
-		s := team.New("m")
+		tm := team.New("m")
+
+		s := session.New()
 
 		u := player.New(
 			"@raspberry", // account
 			"Dima",       // name
 			5,            // id
+			s,
 		)
 
-		err := s.AddUser(u)
+		err := tm.AddUser(u)
 
 		assert.NoError(t, err)
 	})
@@ -31,19 +35,22 @@ func TestTeam(t *testing.T) {
 	t.Run("it does not registers already existed user", func(t *testing.T) {
 		t.Parallel()
 
-		s := team.New("m")
+		tm := team.New("m")
+
+		s := session.New()
 
 		u := player.New(
 			"@raspberry", // account
 			"Dima",       // name
 			5,            // id
+			s,
 		)
 
-		err := s.AddUser(u)
+		err := tm.AddUser(u)
 
 		require.NoError(t, err)
 
-		err = s.AddUser(u)
+		err = tm.AddUser(u)
 
 		assert.Error(t, err, "already existed user was registered again")
 	})
