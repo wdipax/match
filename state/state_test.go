c17bd5db7d1c38ae21645a10ffb3c5ba1e43cfde
Your Name <you@example.com>
2024-04-06 16:28:19 +0400

    describe the game

--- state/state_test.go
+++ state/state_test.go
@@ -18,6 +18,28 @@ func TestState(t *testing.T) {
 
 		s.Process(u)
 	})
+
+	// The game.
+	// Add the Male team.
+	// Open registration to the Male team.
+	// Add members to the Male team.
+	// Close registration to the Male team.
+	// Add Female team.
+	// Open registration to the Female team.
+	// Add members to the Female team.
+	// Close registration to the Female team.
+	// Know each other.
+	// Vote for each other.
+	// Receive matches.
+	// Delete data.
+
+	// Actions.
+	// Start/End a session. [admin]
+	// CRUD Team. [admin]
+	// Open/close registration to the team. [admin]
+	// CRUD Team member [admin,user]
+	// Vote. [user]
+	// Receive matches. [user]
 }
 
 type fakeUpdate struct{}
