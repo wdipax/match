de8b97a036347dae54ca87bcaf184f61540f4380
Your Name <you@example.com>
2024-04-08 11:55:14 +0400

    remove core objects from state

--- state/state.go
+++ state/state.go
@@ -3,10 +3,6 @@ package state
 import (
 	"fmt"
 	"slices"
-
-	"github.com/google/uuid"
-	"github.com/wdipax/match/core/session"
-	"github.com/wdipax/match/core/team"
 )
 
 // State manages relationships between objects.
@@ -14,24 +10,16 @@ type State struct {
 	admins         []string
 	sessionHandler SessionHandler
 
-	adminSession             map[string]string
-	sessions                 map[string]*session.Session
-	sessionTeams             map[string]*mfTeams
-	registrationTokenForTeam map[string]*team.Team
+	userSession map[string]string
+	teams       []string
 }
 
-type mfTeams struct {
-	males   *team.Team
-	females *team.Team
-}
-
-// SessionHandler interacts with core session.
-// TODO: make it responsible for interacting wit core session.
+// SessionHandler interacts with the application core.
 type SessionHandler interface {
 	New() string
-	Delete(id string)
+	Delete(sessionID string)
 	Help(admin bool) string
-	StartRegistration(teamID string) (string, error)
+	StartMaleRegistration(sessionID string) (string, error)
 }
 
 type Settings struct {
@@ -44,8 +32,7 @@ func New(settings *Settings) *State {
 		admins:         settings.Admins,
 		sessionHandler: settings.SessionHandler,
 
-		adminSession: make(map[string]string),
-		sessions:     make(map[string]*session.Session),
+		userSession: make(map[string]string),
 	}
 }
 
@@ -54,42 +41,17 @@ func (s *State) NewSession(userID string) error {
 		return fmt.Errorf("user is not an admin")
 	}
 
-	if _, exists := s.adminSession[userID]; exists {
-		return fmt.Errorf("a session already exists")
+	if _, exists := s.userSession[userID]; exists {
+		return fmt.Errorf("session already exists")
 	}
 
-	sessionID := s.sessionHandler.New()
-
-	s.adminSession[userID] = sessionID
-
-	se := session.New()
-
-	s.sessions[sessionID] = se
-
-	males := team.New("males", se)
-
-	err := se.AddTeam(males)
-	if err != nil {
-		return fmt.Errorf("creating males team: %w", err)
-	}
-
-	females := team.New("females", se)
-
-	err = se.AddTeam(females)
-	if err != nil {
-		return fmt.Errorf("creating females team: %w", err)
-	}
-
-	s.sessionTeams[sessionID] = &mfTeams{
-		males:   males,
-		females: females,
-	}
+	s.userSession[userID] = s.sessionHandler.New()
 
 	return nil
 }
 
-func (s *State) EndSession(id string) {
-	s.sessionHandler.Delete(id)
+func (s *State) EndSession(sessionID string) {
+	s.sessionHandler.Delete(sessionID)
 }
 
 func (s *State) Help(userID string) string {
@@ -101,21 +63,19 @@ func (s *State) StartMaleRegistration(userID string) (string, error) {
 		return "", fmt.Errorf("user is not an admin")
 	}
 
-	sessionID, exists := s.adminSession[userID]
+	sessionID, exists := s.userSession[userID]
 	if !exists {
-		return "", fmt.Errorf("admin does not have an active session")
+		return "", fmt.Errorf("user does not have an active session")
 	}
 
-	teams, exists := s.sessionTeams[sessionID]
-	if !exists {
-		return "", fmt.Errorf("session does not have teams")
+	teamID, err := s.sessionHandler.StartMaleRegistration(sessionID)
+	if err != nil {
+		return "", fmt.Errorf("starting male registration: %w", err)
 	}
 
-	token := uuid.NewString()
+	s.teams = append(s.teams, teamID)
 
-	s.registrationTokenForTeam[token] = teams.males
-
-	return token, nil
+	return teamID, nil
 }
 
 func (s *State) isAdmin(userID string) bool {
